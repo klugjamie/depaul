91b39a2c7194f60d20f31952b4916f5730580bca
klugjamie <klugjamie@gmail.com>
2014-02-02 17:36:31 -0500

    ChangeManager Programs
    initial upload

---
+++ ChangeManager/ChangeManager-web/src/main/java/se554/bean/ChangeManagerBean.java
@@ -0,0 +1,33 @@
+package se554.bean;
+
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import se554.ChangeManager.persistence.ChangeManager;
+
+
+@SessionScoped
+@Named
+public class ChangeManagerBean implements Serializable {
+    private static final Logger logger = Logger.getLogger(ChangeManagerBean.class.getName());
+    
+    @PersistenceContext(unitName = "changemanagerPU")
+    private EntityManager entityManager;
+    
+    public List<ChangeManager> getChangeManagerList() throws SQLException {
+        
+        logger.info("Before getting connection");
+        
+        List<ChangeManager> list = entityManager.createQuery("select a from ChangeManager a").getResultList();
+        
+        logger.log(Level.INFO, "Before returning: {0}", list.size());
+        
+        return list;
+    }
+}
